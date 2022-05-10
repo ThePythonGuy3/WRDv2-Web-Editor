eb39fd1a7f40681503ce7b7366fa90741e756161
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2022-05-10 19:18:30 +0200

    more

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 let size = [50, 36];
 let opening = 2; // *2
 
-let isOpening = (x, y) => {
-	if((x == 0 || x == size[0] - 1) && y >= size[1] / 2 - opening && y <= size[1] / 2 + opening - 1) return x == 0 ? 2 : 4;
-
-	if((y == 0 || y == size[1] - 1) && x >= size[0] / 2 - opening && x <= size[0] / 2 + opening - 1) return y == 0 ? 3 : 1;
-
-	return 0;
-}
-
 let element = id => {
 	return document.getElementById(id);
 }
@@ -21,25 +13,156 @@ let create = type => {
 	return document.createElement(type);
 }
 
-let actOnPath = (path, hide) => {
+let isOpening = (x, y) => {
+	if((x == 0 || x == size[0] - 1) && y >= size[1] / 2 - opening && y <= size[1] / 2 + opening - 1) return x == 0 ? 2 : 4;
+
+	if((y == 0 || y == size[1] - 1) && x >= size[0] / 2 - opening && x <= size[0] / 2 + opening - 1) return y == 0 ? 3 : 1;
+
+	return 0;
+}
+
+let clearFloor = (tiles, x, y) => {
+	for(let x2 = 0; x2 < 4; x2++) {
+		let tile = tiles[getPos(x + x2, y + 1)];
+		tile.setAttribute("block", "F");
+		tile.children[0].src = "./resources/tiles/F.png";
+	}
+
+	updateFloor(tiles, x - 1, y + 1);
+	updateFloor(tiles, x + 4, y + 1);
+}
+
+let actOnPath = (tiles, path, hide) => {
 	let elems = classes("tilePath");
 
 	for(let i = 0; i < elems.length; i++){
 		if(elems[i].getAttribute("wall") == path) {
 			elems[i].className = "tile tilePath";
-			if(hide) elems[i].className += " activePath";
+			if(hide){
+				elems[i].className += " activePath";
+				elems[i].children[0].src = "./resources/tiles/F.png";
+				elems[i].setAttribute("block", "B");
+
+				let x = elems[i].getAttribute("x");
+				let y = elems[i].getAttribute("y");
+				for(let x2 = -1; x2 < 2; x2++) {
+					for(let y2 = -1; y2 < 2; y2++) {
+						let pos = getPos(x - 1 + x2, y - 1 + y2);
+						if(inRange(pos)){ 
+							updateTile(tiles, x - 1 + x2, y - 1 + y2);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	clearFloor(tiles, elems[0].getAttribute("x"), elems[0].getAttribute("y"));
+}
+
+let getPos = (x, y) => {
+	if(x < 0 || x >= size[0] || y < 0 || y >= size[1]) return -1;
+	return x + y * size[0];
+}
+
+let inRange = p => {
+	return p >= 0 && p < 1800;
+}
+
+let updateTile = (tiles, x, y) => {
+	let i = getPos(x, y);
+	let t = tiles[i].getAttribute("block");
+	if(t != "W") return;
+
+	let o = "";
+
+	let p = getPos(x, y + 1);
+	if(inRange(p)){
+		if(tiles[p].getAttribute("block") == "W") o += "D";
+	}
+	p = getPos(x - 1, y);
+	if(inRange(p)){
+		if(tiles[p].getAttribute("block") == "W") o += "L";
+	}
+	p = getPos(x, y - 1);
+	if(inRange(p)){
+		if(tiles[p].getAttribute("block") == "W") o += "U";
+	}
+	p = getPos(x + 1, y);
+	if(inRange(p)){
+		if(tiles[p].getAttribute("block") == "W") o += "R";
+	}
+
+	if(o == "") o = "C";
+
+	tiles[i].children[0].src = "./resources/tiles/" + o + ".png";
+}
+
+let updateFloor = (tiles, x, y) => {
+	let tile = tiles[getPos(x, y)];
+
+	if(tile == undefined) return;
+
+	if(tile.getAttribute("block") == "W" || (inRange(getPos(x, y - 1)) && tiles[getPos(x, y - 1)].getAttribute("block") != "W")) return;
+
+	let o = "s";
+
+	let p = getPos(x - 1, y);
+	if(inRange(p)){
+		if(tiles[getPos(x - 1, y)].getAttribute("block") == "s") o += "L";
+	}
+	p = getPos(x + 1, y);
+	if(inRange(p)){
+		if(tiles[getPos(x + 1, y)].getAttribute("block") == "s") o += "R";
+	}
+
+	if(o == "s") o = "sC";
+
+	tile.children[0].src = "./resources/tiles/" + o + ".png";
+}
+
+let paintTile = (tiles, erase, x, y) => {
+	let i = getPos(x - 1, y - 1);
+	if(!tiles[i].className.includes("activePath")){
+		tiles[i].setAttribute("block", erase ? "B" : "W");
+		let under = getPos(x - 1, y);
+
+		for(let x2 = -1; x2 < 2; x2++) {
+			for(let y2 = -1; y2 < 2; y2++) {
+				let pos = getPos(x - 1 + x2, y - 1 + y2);
+				if(inRange(pos)){ 
+					updateTile(tiles, x - 1 + x2, y - 1 + y2);
+				}
+			}
+		}
+
+		if(!inRange(under)) return;
+		if(tiles[under].getAttribute("block") != "W"){
+			tiles[under].setAttribute("block", "s")
+		}
+
+		if(tiles[getPos(x - 1, y)].getAttribute("block") != "W") {
+			for(let x2 = -1; x2 < 2; x2++) {
+				updateFloor(tiles, x - 1 + x2, y);
+			}
 		}
 	}
 }
 
-let paintTile = (tiles, x, y) => {
-	for(let i = 0; i < tiles.length; i++){
-		if(tiles[i].getAttribute("x") == x - 1 && tiles[i].getAttribute("y") == y - 1){
-			if(tiles[i].className.includes("activePath")) break;
-			tiles[i].children[0].src = "./resources/tiles/C.png";
-			break;
-		}
-	}
+let clearTools = root => {
+	root.style.setProperty("--brush-color", "#BEBEBE");
+	root.style.setProperty("--line-color", "#BEBEBE");
+	root.style.setProperty("--rect-color", "#BEBEBE");
+	root.style.setProperty("--fill-color", "#BEBEBE");
+	root.style.setProperty("--select-color", "#BEBEBE");
+}
+
+let swapMode = (root, nightCheck) => {
+	root.style.setProperty("--bg-color", nightCheck.checked ? "#0D1117" : "#FEFEFE");
+	root.style.setProperty("--text-color", nightCheck.checked ? "#DEDEDE" : "#303030");
+
+	root.style.setProperty("--button-bg-hover", nightCheck.checked ? "#DEDEDE" : "#AEAEAE");
+	root.style.setProperty("--button-bg-active", nightCheck.checked ? "#FEFEFE" : "#8E8E8E");
 }
 
 let zoom = 1;
@@ -47,18 +170,16 @@ const z_speed = 0.2;
 
 let tool = 1;
 
-let clearTools = root => {
-	root.style.setProperty("--brush-color", "#BEBEBE");
-	root.style.setProperty("--line-color", "#BEBEBE");
-	root.style.setProperty("--rect-color", "#BEBEBE");
-	root.style.setProperty("--select-color", "#BEBEBE");
-};
+let erase = false;
 
+let initTheme = true;
 window.onload = () => {
 	let root = document.querySelector(":root");
 
 	let mapContainer = element("mapContainer");
 	let nightCheck = element("nightCheck");
+
+	mapContainer.addEventListener('contextmenu', event => event.preventDefault());
 
 	let down = element("D");
 	let left = element("L");
@@ -70,22 +191,28 @@ window.onload = () => {
 	let brush = element("brush");
 	let line = element("line");
 	let rect = element("rect");
+	let fill = element("fill");
 	let select = element("select");
 
+	if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+		nightCheck.checked = true;
+		swapMode(root, nightCheck);
+	}
+
 	down.onchange = () => {
-		actOnPath(1, down.checked);
+		actOnPath(tiles, 1, down.checked);
 	}
 
 	left.onchange = () => {
-		actOnPath(2, left.checked);
+		actOnPath(tiles, 2, left.checked);
 	}
 
 	up.onchange = () => {
-		actOnPath(3, up.checked);
+		actOnPath(tiles, 3, up.checked);
 	}
 
 	right.onchange = () => {
-		actOnPath(4, right.checked);
+		actOnPath(tiles, 4, right.checked);
 	}
 
 	for(let i = 0; i < buttons.length; i++){
@@ -117,8 +244,16 @@ window.onload = () => {
 		root.style.setProperty("--rect-color", getComputedStyle(root).getPropertyValue("--button-bg-active"));
 	}
 
-	select.onclick = () => {
+	fill.onclick = () => {
 		tool = 4;
+
+		clearTools(root);
+
+		root.style.setProperty("--fill-color", getComputedStyle(fill).getPropertyValue("--button-bg-active"));
+	}
+
+	select.onclick = () => {
+		tool = 5;
 
 		clearTools(root);
 
@@ -126,11 +261,7 @@ window.onload = () => {
 	}
 
 	nightCheck.onchange = () => {
-		root.style.setProperty("--bg-color", nightCheck.checked ? "#0D1117" : "#FEFEFE");
-		root.style.setProperty("--text-color", nightCheck.checked ? "#DEDEDE" : "#303030");
-
-		root.style.setProperty("--button-bg-hover", nightCheck.checked ? "#DEDEDE" : "#AEAEAE");
-		root.style.setProperty("--button-bg-active", nightCheck.checked ? "#FEFEFE" : "#8E8E8E");
+		swapMode(root, nightCheck);
 	}
 
 
@@ -157,6 +288,8 @@ window.onload = () => {
 				n.setAttribute("wall", 0);
 			}
 
+			n.setAttribute("block", "B");
+
 			n.style.left = x * 12 + 4 + "px";
 			n.style.top = y * 12 + "px";
 
@@ -178,17 +311,21 @@ window.onload = () => {
 		clicked = false;
 	});
 
+	mapContainer.addEventListener("pointerleave", e => {
+		clicked = false;
+	});
+
 	mapContainer.addEventListener("pointermove", e => {
 		if(clicked) {
 			let rect = mapContainer.getBoundingClientRect();
 
-			let x = parseInt((parseInt(e.clientX - rect.left) - 3) / 12) + 1;
-			let y = parseInt((parseInt(e.clientY - rect.top) - 3) / 12) + 1;
+			let x = parseInt((parseInt(e.clientX - rect.left) - 4) / 12) + 1;
+			let y = parseInt((parseInt(e.clientY - rect.top)) / 12) + 1;
 			
 			if(x > 50) x = 50;
 			if(y > 36) y = 36;
 
-			paintTile(tiles, x, y);
+			paintTile(tiles, erase, x, y);
 		}
 	});
 }
